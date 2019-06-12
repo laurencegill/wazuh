730d18f0d635463eeac40feaea51a28f9591f4b8
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-06-12 12:02:28 +0200

    Add auth to kwargs in controllers

--- api/api/controllers/syscheck_controller.py
+++ api/api/controllers/syscheck_controller.py
@@ -51,8 +51,7 @@ def put_syscheck_agent(agent_id, pretty=False, wait_for_complete=False):
     :param agent_id: Agent ID
     :type agent_id: str
     """
-    f_kwargs = {'agent_id': agent_id,
-                'auth': connexion.request.headers['Authorization']}
+    f_kwargs = {'agent_id': agent_id, 'auth': connexion.request.headers['Authorization']}
 
     dapi = DistributedAPI(f=syscheck.run,
                           f_kwargs=remove_nones_to_dict(f_kwargs),
@@ -68,10 +67,10 @@ def put_syscheck_agent(agent_id, pretty=False, wait_for_complete=False):
 
 
 @exception_handler
-def get_syscheck_agent(agent_id, pretty=False, wait_for_complete=False, offset=0, 
-                       limit=None, select=None, sort=None, search=None,
-                       summary=False, md5=None, sha1=None, sha256=None):
+def get_syscheck_agent(agent_id, pretty=False, wait_for_complete=False, offset=0, limit=None, select=None, sort=None,
+                       search=None, summary=False, md5=None, sha1=None, sha256=None):
     """
+
     :param agent_id: Agent ID
     :type agent_id: str
     :param pretty: Show results in human-readable format 
@@ -84,7 +83,8 @@ def get_syscheck_agent(agent_id, pretty=False, wait_for_complete=False, offset=0
     :type limit: int
     :param select: Select which fields to return (separated by comma)
     :type select: List[str]
-    :param sort: Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in ascending or descending order. 
+    :param sort: Sorts the collection by a field or fields (separated by comma). Use +/- at the beginning to list in
+    ascending or descending order.
     :type sort: str
     :param search: Looks for elements with the specified string
     :type search: str
@@ -97,20 +97,16 @@ def get_syscheck_agent(agent_id, pretty=False, wait_for_complete=False, offset=0
     :param sha256: Filters files with the specified SHA256 checksum.
     :type sha256: str
     """
-
-    # get type parameter from query
+    # get parameters from query
     type_ = connexion.request.args.get('type', None)
-    # get hash parameter from query
     hash_ = connexion.request.args.get('hash', None)
-    # get file parameter from query
     file_ = connexion.request.args.get('file', None)
 
-    filters = {'type': type_, 'md5': md5, 'sha1': sha1,
-               'sha256': sha256, 'hash': hash_, 'file': file_}
+    filters = {'type': type_, 'md5': md5, 'sha1': sha1, 'sha256': sha256, 'hash': hash_, 'file': file_}
 
-    f_kwargs = {'agent_id': agent_id, 'offset': offset, 'limit': limit,
-                'select': select, 'sort': parse_api_param(sort, 'sort'), 'search': parse_api_param(search, 'search'),
-                'summary': summary, 'filters': filters}
+    f_kwargs = {'agent_id': agent_id, 'offset': offset, 'limit': limit, 'select': select,
+                'sort': parse_api_param(sort, 'sort'), 'search': parse_api_param(search, 'search'), 'summary': summary,
+                'filters': filters, 'auth': connexion.request.headers['Authorization']}
 
     dapi = DistributedAPI(f=syscheck.files,
                           f_kwargs=remove_nones_to_dict(f_kwargs),
@@ -137,7 +133,7 @@ def delete_syscheck_agent(agent_id, pretty=False, wait_for_complete=False):
     :param agent_id: Agent ID
     :type agent_id: str
     """
-    f_kwargs = {'agent_id': agent_id}
+    f_kwargs = {'agent_id': agent_id, 'auth': connexion.request.headers['Authorization']}
 
     dapi = DistributedAPI(f=syscheck.clear,
                           f_kwargs=remove_nones_to_dict(f_kwargs),
@@ -163,7 +159,7 @@ def get_last_scan_agent(agent_id, pretty=False, wait_for_complete=False):
     :param agent_id: Agent ID
     :type agent_id: str
     """
-    f_kwargs = {'agent_id': agent_id}
+    f_kwargs = {'agent_id': agent_id, 'auth': connexion.request.headers['Authorization']}
 
     dapi = DistributedAPI(f=syscheck.last_scan,
                           f_kwargs=remove_nones_to_dict(f_kwargs),
@@ -174,14 +170,6 @@ def get_last_scan_agent(agent_id, pretty=False, wait_for_complete=False):
                           logger=logger
                           )
     data = raise_if_exc(loop.run_until_complete(dapi.distribute_function()))
-
-    # Check if scan is running to set end to None
-    if data["start"] is not None:
-        start = data["start"]
-        end = data["end"]
-        if end is not None and start > end:
-            data["end"] = None
-
     response = Data(data)
 
     return response, 200
