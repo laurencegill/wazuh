01a35b03b71b39da4c81c5a3f9b2d575dff7d103
David Iglesias Lopez <davidjiglesias@wazuh.com>
2019-06-12 12:04:25 +0200

    Update rbac module

--- framework/wazuh/rbac.py
+++ framework/wazuh/rbac.py
@@ -24,14 +24,14 @@ def get_user_permissions(**kwargs):
 def get_required_permissions(actions: list = None, resources: str = None, *args, **kwargs):
 
     # We expose required resources for the request
-    m = re.search("^(\w+\:\w+:){(\w+)}$", resources)
+    m = re.search(r'^(\w+\:\w+:)(\w+|\*|{(\w+)})$', resources)
     res_list = list()
-    # If we find a regex match we obtain the dynamic resource/s
-    if m:
+    res_base = m.group(1)
+    # If we find a '{' in the regex we obtain the dynamic resource/s
+    if '{' in m.group(2):
         try:
-            res_base = m.group(1)
             # Dynamic resources ids are found within the {}
-            params = kwargs[m.group(2)]
+            params = kwargs[m.group(3)]
             # We check if params is a list of resources or a single one in a string
             if isinstance(params, list):
                 for param in params:
@@ -45,10 +45,10 @@ def get_required_permissions(actions: list = None, resources: str = None, *args,
     else:
         res_list.append(resources)
 
-    # Create dict of required policies with action:set(resources) pairs
+    # Create dict of required policies with action: list(resources) pairs
     req_permissions = dict()
     for action in actions:
-        req_permissions[action] = set(res_list)
+        req_permissions[action] = res_list
 
     return req_permissions
 
@@ -57,28 +57,33 @@ def match_pairs(mode: bool = False, user_permissions: list = None, req_permissio
 
     # We run through all required permissions for the request
     for req_action, req_resources in req_permissions.items():
-        # allow_match is used to keep track when a required permission is matched by a policy with an allow effect
+        # allow_match is used to keep track when a required permission is matched by a policy with an allowed effect
         allow_match = False
         # We run through the user permissions to find a match with the required permissions
         for policy in user_permissions:
-            # We find if action and resources match
+            # We find if action matches
             action_match = req_action in policy['actions']
-            res_match = req_resources.issubset(policy['resources'])
-            # When any policy with a deny effect matches, we deny the request directly
-            if action_match and res_match and policy['effect'] == "deny":
-                return False
-            # When any policy with an allow effect matches, we set a match in allow_match and
-            # break out to continue with required permissions
-            elif action_match and res_match and policy['effect'] == "allow":
-                allow_match = True
-                break
+            if action_match:
+                for req_resource in req_resources:
+                    # We find resource name to add * if not already there
+                    m = re.search(r'^(\w+\:\w+:)(\w+)$', req_resource)
+                    # We find if resource matches
+                    if m is not None:
+                        req_asterisk = '{0}{1}'.format(m.group(1), '*')
+                        res_match = (req_resource or req_asterisk) in policy['resources']
+                    else:
+                        res_match = req_resource in policy['resources']
+                    # When any policy with a deny effect matches, we deny the request directly
+                    if res_match and policy['effect'] == "deny":
+                        return False
+                    # When any policy with an allow effect matches, we set a match in allow_match and
+                    # break out to continue with required permissions
+                    elif res_match and policy['effect'] == "allow":
+                        allow_match = True
+                        break
             # We continue running through the user permissions if no match is found in actual policy
-        # If we have an allow match or we are using black list mode we continue with next required permission
-        if allow_match or mode:
-            continue
-        # Otherwise, if we are using white list mode and no match is found for the required permission
-        # we deny the request
-        else:
+        # If we are using white list mode and no match is found for the required permission we deny the request.
+        if not allow_match and not mode:
             return False
     # If we don't find a deny match or we find an allow match for all policies in white list mode we allow the request
     return True
